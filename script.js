cc64ba0e5df56a8a54010128cc52ce269e9a9fac
Joao-Vitor-Siqueira <jvitorsiqueira99@gmail.com>
2023-10-17 21:54:32 -0300

    trabalho finalizado

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const formPcp = document.getElementById("formPcp")
 const finishBtn = document.getElementById("finish")
 const log = document.getElementById("log")
 const resultsTable = document.getElementById("resultsTable")
+const resultsRequisitos = document.getElementById("resultEr").children
+const resultsPcp = document.getElementById("resultPcp").children
 
 const itensRequisitos = [
     "Foi gerado um artefato contendo os requisitos coletados?",
@@ -16,8 +18,18 @@ const itensRequisitos = [
     "Os requisitos coletados possuem classificação com base em sua prioridade?",
 ]
 
+const itensIntegracao = [
+    "Foi gerado um diagrama de classes?",
+    "Foi gerado um diagrama de componentes?",
+    "Foi gerado um artefato baseado nos diagramas?",
+    "O artefato gerado é mantido atualizado?",
+    "Os diagramas estão de acordo com as boas práticas da UML?",
+    "O produto gerado segue a estrutura planejada anteriormente?",
+    "Os diagramas usam os recursos humanos de maneira eficiente? ",
+    "Foi feita a revisão do produto após a integração?"
+]
 
-function loadFormRequisitos(form,arr){
+function loadForm(form,arr){
     let idTemplate;
     form.id == "formRequisitos"?idTemplate = "er": idTemplate = "pcp"
   
@@ -54,43 +66,46 @@ function loadFormRequisitos(form,arr){
         }
 }
 
-function generateLog(){
-    for (let i = 0; i < itensRequisitos.length; i++) {
+function generateLog(idTemplate,itens,resultDiv,processo){
+    let nConformidades = 0; 
+    for (let i = 0; i < itens.length; i++) {
         let newRow = document.createElement('tr');
-        let select = document.getElementById(`er${i}Select`)
-        let checkbox = document.getElementById(`er${i}`); 
+        let select = document.getElementById(`${idTemplate + i}Select`)
+        let checkbox = document.getElementById(`${idTemplate + i}`); 
         if(!checkbox.checked){
            continue
-        }  
-    let desc = document.createElement("td")
-        desc.textContent = itensRequisitos[i]
-        let processo = document.createElement("td")
-        processo.textContent = "Engenharia de requisitos"
+        } 
+        nConformidades ++; 
+        let desc = document.createElement("td")
+        desc.textContent = itens[i]
+        let processoCol = document.createElement("td")
+        processoCol.textContent = processo
         let prioridade = document.createElement("td")
         prioridade.textContent = select.options[select.selectedIndex].value
 
         newRow.appendChild(desc)
-        newRow.appendChild(processo)
+        newRow.appendChild(processoCol)
         newRow.appendChild(prioridade)
-
-        resultsTable.appendChild(newRow);
-       
+        resultsTable.appendChild(newRow);      
     }
 
-    
-    
+    resultDiv[1].innerHTML += itens.length;
+    resultDiv[2].innerHTML += nConformidades
+    resultDiv[3].innerHTML += Math.round(Math.abs((nConformidades - itens.length) / itens.length * 100)).toFixed(2) + "%"  
 }
 
 
 
 document.addEventListener("DOMContentLoaded", function() {
-    loadFormRequisitos(formRequisitos,itensRequisitos)
-    loadFormRequisitos(formPcp,itensRequisitos)
+    loadForm(formRequisitos,itensRequisitos)
+    loadForm(formPcp,itensIntegracao)
   });
 
 finishBtn.addEventListener("click",(e) =>{
     formsDiv.classList.add("hidden")
     finishBtn.classList.add("hidden")
     log.classList.remove("hidden")
-    generateLog()
+    generateLog("er",itensRequisitos,resultsRequisitos,"Engenharia de requisitos")
+    generateLog("pcp",itensIntegracao,resultsPcp,"Intregação do produto")
 })
+
